1a4bf5070fb148a315096cf4ab559e85eef923ef
Greg White <gregwhite90@gmail.com>
2018-06-19 19:39:20 -0700

    Enable API request to be run in background asynchronously

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 from flask import Flask, render_template
+import tasks
 
 app = Flask(__name__)
-
-from rq import Queue
-from worker import conn
-import utils
-
-q = Queue(connection=conn)
 
 @app.route('/')
 def homepage():
     query = 'Cruz, Rafael'
-    query_response = q.enqueue(utils.fec_request, 'candidates/search/', query).text
+    query_response = tasks.fec_request('candidates/search/', query).text
     return render_template('index.html',
                            query=query,
                            query_response=query_response)
--- tasks.py
+++ tasks.py
@@ -1,6 +1,10 @@
+import celery
 import requests
 import os
 
+app = celery.Celery('stay-bought')
+
+@app.task
 def fec_request(endpoint, query):
     payload = {
         'api_key': os.environ.get('DATAGOV_API_KEY'), # to add default?
@@ -11,3 +15,8 @@ def fec_request(endpoint, query):
     base_uri = 'https://api.open.fec.gov/v1/'
     r = requests.get(base_uri + endpoint, params=payload)
     return r
+
+redis_url = os.environ.get('REDIS_URL') # to add default?
+
+app.conf.update(BROKER_URL=redis_url,
+                CELERY_RESULT_BACKEND=redis_url)
